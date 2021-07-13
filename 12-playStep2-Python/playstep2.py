1499a300760f7c70197b64cd4ff842df0d943ce6
Mohan kandregula <71484692+Mohankandregula-pixel@users.noreply.github.com>
2021-07-14 02:06:47 +0530

    playstep2_mohan

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -35,4 +35,72 @@
 
 def playstep2(hand, dice):
 	# your code goes here
-	pass
+	a = hand//100
+	b = (hand//10)%10
+	c = hand%10
+	mohan = str(dice)
+	len(str(mohan))
+	print(len(mohan))
+	if len(mohan)==2:
+		d = dice//10
+		e = dice%10
+		check = [d ,e]
+		r = 0
+		
+	elif len(mohan)==3:
+		d = dice//100
+		e = (dice//10)%10
+		f = dice%10
+		check = [d ,e, f]
+		r = dice//10
+	else:
+		d = dice//1000
+		e = (dice//100)%10
+		f = (dice//10)%10
+		g = (dice%10)
+		check = [d ,e, f, g]
+		r = dice//100
+
+	# print(a,b,c,d,e,f,g)
+	if a==b==c:
+ 		return (hand, dice)
+	elif a == b:
+		r = dice//10
+		k = [a,b,check[-1]]
+		k.sort(reverse=True)
+		m = str(k[0])
+		m += str (k[1])
+		m += str (k[2])
+		n = int(m)
+		return (n,r)
+	elif b == c:
+		r = dice//10
+		k = [b,c,check[-1]]
+		k.sort(reverse=True)
+		m = str(k[0])
+		m += str (k[1])
+		m += str (k[2])
+		n = int(m)
+		return (n,r)
+	elif a == c:
+		r = dice//10
+		k = [a,c,check[-1]]
+		k.sort(reverse=True)
+		m = str(k[0])
+		m += str (k[1])
+		m += str (k[2])
+		n = int(m)
+		return (n,r)
+	elif a != b or b != c or c != a:
+		k = [a,check[-1],check[-2]]
+		k.sort(reverse=True)
+  
+		m = str(k[0])
+		m += str (k[1])
+		m += str (k[2])
+		n = int(m)
+		return (n,r)
+
+
+print(playstep2(442,3612))
+
