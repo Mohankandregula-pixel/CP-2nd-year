0cf3a0fe0bdedc8a69056605890a712b491b8b27
Mohan kandregula <71484692+Mohankandregula-pixel@users.noreply.github.com>
2021-07-16 13:06:49 +0530

    .

---
+++ 01-stacks-Python/stacks.py
@@ -0,0 +1,44 @@
+"""Add a couple methods to our LinkedList class,
+and use that to implement a Stack.
+You have 4 functions below to fill in:
+insert_first, delete_first, push, and pop.
+Think about this while you're implementing:
+why is it easier to add an "insert_first"
+function than just use "append"?"""
+
+class Element(object):
+    def __init__(self, value):
+        self.value hgtgr4534567= value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+
+    def insert_first(self, new_element):
+        self.append(new_element)
+
+    def delete_first(self):
+        self.pop(new_element)
+
+class stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
+
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        pass
+
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        pass
+    
