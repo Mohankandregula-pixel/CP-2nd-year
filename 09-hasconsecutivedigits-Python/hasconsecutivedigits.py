8f80673876d0af9d3383261616f9c361e7514b16
Mohan kandregula <71484692+Mohankandregula-pixel@users.noreply.github.com>
2021-07-19 15:23:35 +0530

    mohan

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -5,25 +5,32 @@
 def hasconsecutivedigits(n):
 	# your code goes here
 	n = abs(n)
-	if n<100:
-		return False
-	res = list(map(int, str(n)))
-	# sorted_list = sorted(res)
+	p = -1
+	while n != 0 :
+		k = n%10
+		if k == p:
+			return True
+		p = k
+		n = n//10
+	return False
 
-	# range_list=list(range(min(res), max(res)+1))
-	count = 0
-	m = 0
-	for i in res:
-		if (i == res[m]):
-			count += 1
-			m += 1
-	if n <= 10:
-		return False
-	elif count == 0 and n//10 == n%10:
-		return True
-	elif count > 1 :
-		return True 
-	else:
-		return False
 
-print(hasconsecutivedigits(-24))
+	# if n<100:
+	# 	return False
+	# res = list(map(int, str(n)))
+	# count = 0
+	# m = 0
+	# for i in res:
+	# 	if (i == res[m]):
+	# 		count += 1
+	# 		m += 1
+	# if n <= 10:
+	# 	return False
+	# elif count == 0 and n//10 == n%10:
+	# 	return True
+	# elif count > 1 :
+	# 	return True 
+	# else:
+	# 	return False
+
+print(hasconsecutivedigits(224))
